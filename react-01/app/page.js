bfdb27fb72e1feed04b0b057d7e7023c128ae649
Maria Clara Ribeiro <maria.00000854922@unicap.br>
2026-08-11 11:25:29 +0000

    alteracoes-01

--- react-01/app/page.js
+++ react-01/app/page.js
@@ -8,35 +8,35 @@ export default function Home() {
         <Image
           className={styles.logo}
           src="/next.svg"
-          alt="Next.js logo"
+          alt="Home Page"
           width={100}
           height={20}
           priority
         />
         <div className={styles.intro}>
           <h1>
-            Clary's App 
+            Clary's 
 
-            <code className={styles.code}>page.js</code> file.
+            <code className={styles.code}>App</code>
           </h1>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
+            
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Templates
+              
             </a>{" "}
-            or the{" "}
+            
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learning
+             
             </a>{" "}
-            center.
+            
           </p>
         </div>
         <div className={styles.ctas}>
@@ -53,7 +53,7 @@ export default function Home() {
               width={16}
               height={14}
             />
-            Deploy Now
+            Adicione um comentário
           </a>
           <a
             className={styles.secondary}
