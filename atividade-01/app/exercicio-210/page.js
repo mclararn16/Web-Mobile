3bf98ca871fc0e3015957266f6f92b8203a187c5
Maria Clara Ribeiro <maria.00000854922@unicap.br>
2026-08-25 11:45:02 +0000

    ajuste no exercicio 210

--- atividade-01/app/exercicio-210/page.js
+++ atividade-01/app/exercicio-210/page.js
@@ -2,11 +2,13 @@ export default function Exercicio210() {
   return (
     <div
       style={{
-        backgroundImage: "https://img.lojasrenner.com.br/item/931822635/original/4.jpg",
+        backgroundImage:
+          'url("https://www.popsci.com/wp-content/uploads/2018/12/10/QKOPUFKJMVV7JE2YNBCV2FILFQ.jpg?quality=85&w=2048")',
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         minHeight: "100vh",
+        width: "100%",
         margin: 0,
       }}
     >
@@ -17,6 +19,7 @@ export default function Exercicio210() {
           color: "#e1edd1",
           display: "inline-block",
           padding: "10px",
+          
         }}
       >
         Primeiro exercício de CSS
